9439fc859c177105c647aed2c78e2f720150bf5f
RahmatRiansyah <rahmatriansyahpkp@gmail.com>
2025-01-07 20:56:45 +0700

    Update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let index = 0;
 const motivasi = [
   "Usia 21 adalah awal dari perjalanan yang lebih dewasa.",
   "Teruslah bermimpi dan berusaha!",
@@ -11,17 +12,13 @@ const motivasi = [
   "Jadikan setiap hari berarti.",
 ];
 
-function tampilkanMotivasi() {
-  let index = 0;
+function tampilkanPesan() {
   const pesan = document.getElementById("pesan");
-  function tampilkanPesan() {
-    pesan.innerText = motivasi[index];
-    index++;
-    if (index >= motivasi.length) {
-      index = 0;
-    }
+  pesan.innerText = motivasi[index];
+  index++;
+  if (index >= motivasi.length) {
+    index = 0;
   }
-  setInterval(tampilkanPesan, 2000); // Tampilkan pesan setiap 2 detik
 }
 
-tampilkanMotivasi();
+document.getElementById("tombol").addEventListener("click", tampilkanPesan);
